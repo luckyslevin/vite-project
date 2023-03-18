54209883013a99ba65e9efaa60c157dbee9feed9
McLOVIN <shortcut7777@gmail.com>
2023-03-19 00:22:37 +0800

    chore: add run.bat

---
+++ run.bat
@@ -0,0 +1,13 @@
+@echo off
+
+if "%1" == "install" (
+  docker-compose run --rm app "npm i -g pnpm && pnpm install"
+)
+
+if "%1" == "logs" (
+	docker-compose logs -f
+)
+
+if "%1" == "up" (
+  docker-compose up -d --force-recreate app
+)
